19de00a2d236b546a0fde8426c3f4aab6d066961
Victor Jafet <pl248111@ahschool.com>
2024-11-07 10:35:35 -0500

    this is broken

--- databases/DB.py
+++ databases/DB.py
@@ -13,36 +13,83 @@ class DB:
 
 
 
-    # user functions below
+    def dictify(self, result):
+        dict = [row.__dict__ for row in result]
+
+        for row_dict in dict: #remove useless SQLAlchemy metadata from dictionaries
+            row_dict.pop('_sa_instance_state',None)
+
+        return dict #should only return 1 or 0 elements
+
+    #add update_question and really every other table's update function
+
+
+
     def add_user(self, username: str, email: str, password: str): #adds a user
-        if username and password:
+        if username and email and password:
             new_user = User(username=username, email=email, password=password)
-            self.session.add(new_user) #adds user object
+            self.session.add(new_user)
             self.session.commit() #commits changes to file
+            return new_user.id
         else:
-            print("add_user: Missing username or password, no user added")
+            print("add_user: Missing username, email, or password, no user added")
+            return None
 
     def get_users(self, username: str): #returns a list of dictionaries, one for each user
         #structure = session.query(table object).filter(operation on column object inside table object).all()/.first()
-
         if username:
             result = self.session.query(User).filter(User.username == username).all()
+            if len(result) > 1:
+                print("get_users: More than one user with username " + username + " exists, this shouldn't happen")
+                return None
+            if len(result) < 1:
+                print("get_users: No users exist with user id")
+                return None
+            return self.dictify(result) #returns all users matching the specified query (ideally 0 or 1, can be more but shouldnt)
         else:
-            print("find_user: No username passed in")
-            return []
-
-        user_list = [user.__dict__ for user in result] #converts all returned user objects to dictionary
-
-        for user_dict in user_list: #remove useless SQLAlchemy metadata from dictionaries
-            user_dict.pop('_sa_instance_state',None)
-
-        return user_list #returns all users matching the specified query (ideally 0 or 1, can be more but shouldnt)
+            print("get_users: No username passed in")
+            return None
     
 
 
+    def add_user_stats(self, user_id: str):
+        if user_id:
+            new_user_stats = UserStatistics(user_id=user_id, total_score=0, solved_problems_count=0)
+            self.session.add(new_user_stats) #adds user object
+            self.session.commit() #commits changes to file
+            return new_user_stats.id
+        else:
+            print("add_user_stats: Missing user id, no user stats added")
+            return None
+        
+    def get_user_stats(self, user_id: str): #returns a list of dictionaries, one for each user
+        if user_id:
+            result = self.session.query(UserStatistics).filter(UserStatistics.user_id == user_id).all()
+            return self.dictify(result) #returns all user statistics matching the specified query (ideally 0 or 1, can be more but shouldnt)
+        else:
+            print("get_user_stats: No user id passed in")
+            return None
 
+    def update_user_stats(self, user_id, added_score, added_problem_count):
+        if user_id and added_score != None and added_problem_count != None:
+            user_stats = self.session.query(UserStatistics).filter(UserStatistics.user_id == user_id).all()
+            if len(user_stats) > 1:
+                print("update_user_stats: More than one user stats exist, this shouldn't happen; nothing updated")
+                return None
+            if len(user_stats) < 1:
+                print("update_user_stats: No user stats exist for that user id; nothing updated")
+                return None
+            user_stats = user_stats[0]
+            user_stats.total_score += added_score
+            user_stats.solved_problems_count += added_problem_count
+            self.session.commit() #commits changes to file
+            return user_stats.id
+        else:
+            print("update_user_stats: User id, added score, or added_problem_count not passed in; nothing updated")
+            return None
 
-    # question functions below
+    
+
     def get_questions(self, id: int): #returns a list of dictionaries, one for each question (should only be 1 or 0)
         if id:
             result = self.session.query(Question).filter(Question.id == id).all()
@@ -50,23 +97,18 @@ class DB:
             print("get_question: No id passed in")
             return []
         
-        question_list = [question.__dict__ for question in result]
-
-        for question_dict in question_list: #remove useless SQLAlchemy metadata from dictionaries
-            question_dict.pop('_sa_instance_state',None)
-
-        return question_list #should only return 1 or 0 elements
+        return self.dictify(result) #should only return 1 or 0 elements
 
     def add_question(self, content: str, difficulty: int):
         if content and difficulty != None:
             new_question = Question(content=content, difficulty=difficulty)
             self.session.add(new_question) #adds user object
             self.session.commit()
-            return new_question.id
+            return new_question.id #necessary to link answers and solutions
         else:
             print("add_question: Missing question content or difficulty (should be 0 for undefined), no question added")
 
-    #add update_question and really every other table's update function
+
 
     def add_answer(self, content: str, correct: bool, question_id: int):
         if content and correct != None and question_id: # correct != None because False is falsey value and false triggers null prevention lol
@@ -77,6 +119,17 @@ class DB:
         else:
             print("add_answer: Missing answer content, correct boolean, or question id, no answer added")
 
+    def get_answers(self, question_id: int):
+        if question_id:
+            result = self.session.query(Answer).filter(Answer.question_id == question_id).all()
+        else:
+            print("get_answer: No question_id passed in")
+            return []
+        
+        return self.dictify(result)
+
+
+
     def add_solution(self, content: str, question_id: int):
         if content and question_id:
             new_solution = Solution(content=content, question_id=question_id)
@@ -86,5 +139,51 @@ class DB:
         else:
             print("add_solution: Missing solution content or question id, no solution added")
 
-
+    def get_solutions(self, question_id: int):
+        if question_id:
+            result = self.session.query(Solution).filter(Solution.question_id == question_id).all()
+        else:
+            print("get_solution: No question_id passed in")
+            return []
         
+        return self.dictify(result)
+
+
+
+    def add_user_response(self, user_id: str, question_id: int, answer_id: int, submission_time: int):
+        if user_id and question_id and answer_id and submission_time:
+            new_answer = UserResponse(user_id=user_id, question_id=question_id, answer_id=answer_id, submission_time=submission_time)
+            self.session.add(new_answer)
+            self.session.commit()
+            return new_answer.id
+        else:
+            print("add_user_response: Missing user id, question id, answer id, or submission time, no answer added")
+
+    def get_user_responses(self, user_id: int, question_id: int):
+        if user_id and question_id:
+            result = self.session.query(UserResponse).filter(UserResponse.user_id == user_id and UserResponse.question_id == question_id).all()
+        else:
+            print("get_user_responses: No user id or question id passed in")
+            return []
+        
+        return self.dictify(result)
+    
+
+
+    def add_user_response(self, user_id: str, question_id: int, answer_id: int, submission_time: int):
+        if user_id and question_id and answer_id and submission_time:
+            new_answer = UserResponse(user_id=user_id, question_id=question_id, answer_id=answer_id, submission_time=submission_time)
+            self.session.add(new_answer)
+            self.session.commit()
+            return new_answer.id
+        else:
+            print("add_user_response: Missing user id, question id, answer id, or submission time, no answer added")
+
+    def get_user_responses(self, user_id: int, question_id: int):
+        if user_id and question_id:
+            result = self.session.query(UserResponse).filter(UserResponse.user_id == user_id and UserResponse.question_id == question_id).all()
+        else:
+            print("get_user_responses: No user id or question id passed in")
+            return []
+        
+        return self.dictify(result) #should only return 1 or 0 elements
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-    # run file: uvicorn main:app --reload
+# run file: uvicorn main:app --reload
 
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -10,13 +10,60 @@ from databases.DB import DB #class made to handle all database interaction (2 me
 
 import bcrypt
 
+import jwt
+
 import logging
 
 import time
 
+from datetime import datetime, timedelta, timezone
+
+import os
+
+
+
+
+SECRET_KEY = os.environ.get("PATH", "pathpathpathlol")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 120  # set token expiration time in minutes
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") #generates a dependency that extracts the token from the Authorization header
+
+def make_jwt_token(username: str) -> str:
+    to_encode = {
+        "sub":username #sub means subject, 
+    }
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) #time to expire, adds an amount of time to the current utc time
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Depends(oauth2_scheme) in get_username_from_jwt retrieves the token from the Authorization header (in Bearer format) without needing additional code in the endpoint
+def get_username_from_jwt(token: str = Depends(oauth2_scheme)): #given a 
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return username
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+
 app = FastAPI()
 db = DB()
 # logger = logging.getLogger(__name__) #still working on getting it to log to a file
+
 
 
 '''
@@ -24,6 +71,12 @@ https://gist.github.com/liviaerxin/d320e33cbcddcc5df76dd92948e5be3b
 use this for logging in the
 also refer to test endpoint if needed to get ip/port from request
 '''
+
+
+
+
+
+
 
 
 @app.get("/api/test")
@@ -43,19 +96,22 @@ async def login(user: UserLoginJSON):
         print("Multiple users with the same username " + user.username + " exist")
         return {"success":False,
                 "error_code":"placeholder",
-                "error_message":"Multiple users with the same username " + user.username + " exist"}
+                "error_message":"Invalid credentials"}
     if len(login_user) < 1:
         return {"success":False,
                 "error_code":"placeholder",
-                "error_message":"No user exists"}
+                "error_message":"Invalid credentials"}
     login_user = login_user[0]
     if bcrypt.checkpw(user.password.encode('utf-8'), login_user["password"]):
+        jwt_token = make_jwt_token(login_user["username"])
         return {"success":True,
-                "session_token":"placeholder"}
+                "access_token":jwt_token,
+                "token-type":"bearer"}
     else:
         return {"success":False,
                 "error_code":"placeholder",
-                "error_message":"Incorrect password"} #refer to todo (make sure messages are different due to cybersecurity reasons)
+                "error_message":"Invalid credentials"} #refer to todo (make sure messages are different due to cybersecurity reasons)
+
 
 
 class UserSignUpJSON(BaseModel): #creates a pydantic class that models the expected incoming json for logging in/signing up
@@ -71,8 +127,15 @@ async def signup(user: UserSignUpJSON):
                 "error_message":"Username already exists"}
     else:
         db.add_user(user.username, user.email, bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt()))
-        return {"success":True,
-                "session_token":"placeholder"}
+        return {"success":True}
+
+
+
+
+
+
+
+
 
 
 
@@ -89,9 +152,8 @@ class QuestionAddJSON(BaseModel):
     answers: List[AnswerAddJSON]
     solutions: List[SolutionAddJSON]
 
-
 @app.post("/api/add_question/")
-async def add_question(question: QuestionAddJSON):
+async def add_question(question: QuestionAddJSON, current_user: str = Depends(get_username_from_jwt)):
     answers = []
     for answer in question.answers: #turns all answer json into dict
         answers.append(answer.__dict__)
@@ -99,14 +161,13 @@ async def add_question(question: QuestionAddJSON):
     for solution in question.solutions: #turns all solution json into dict
         solutions.append(solution.__dict__)
     try:
-        question_id = db.add_question(question.content)
-        print(question_id)
+        question_id = db.add_question(question.content, difficulty=question.difficulty)
 
         for answer in question.answers:
             db.add_answer(answer.content, answer.correct, question_id)
 
         for solution in question.solutions:
-            db.add_answer(solution.content, question_id)
+            db.add_solution(solution.content, question_id)
 
         return {"success":True,
                 "message":"placeholder"}
@@ -117,7 +178,7 @@ async def add_question(question: QuestionAddJSON):
     
 
 @app.get("/api/question/{question_id}")
-async def question(question_id: int):
+async def question(question_id: int, current_user: str = Depends(get_username_from_jwt)):
     question = db.get_questions(question_id)
     if len(question) > 1:
         print("Multiple questions with the same id " + str(question_id) + " exist")
@@ -129,8 +190,28 @@ async def question(question_id: int):
                 "error_code":"placeholder",
                 "error_message":"No question exists under id " + str(question_id)}
     question = question[0]
+    # answers = db.get #MAKE A DB INTERACTABLE FUNCTION THAT GETS ANSWERS
     return {"success":True,
-            "content":question["content"]}
+            "content":question["content"],
+            "answers":"placeholder"}
+
+
+
+
+class QuestionAddJSON(BaseModel):
+    content: str
+    difficulty: int
+    answers: List[AnswerAddJSON]
+    solutions: List[SolutionAddJSON]
+
+
+class AnswerSubmissionJSON(BaseModel):
+    answer: str
+
+@app.post("/api/question/{question_id}")
+async def question(question_id: int, current_user: str = Depends(get_username_from_jwt)):
+    pass
+
 
 # do a post for the question endpoint for answering the question
 
